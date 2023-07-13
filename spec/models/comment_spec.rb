0e6243e702a8d7eec7e7069721d04d30960dee23
lucascomamala <lucascomamala@gmail.com>
2023-07-13 12:53:57 -0600

    Comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'John Doe', posts_counter: 0) }
+
+  let(:post) { Post.create(title: 'Title', text: 'Content', comments_counter: 0, likes_counter: 0, author: user) }
+
+  describe '#increment_comments_counter' do
+    it 'increments the comments_counter of the post' do
+      Comment.create(text: 'Content', post:, user:)
+      expect(post.comments_counter).to eq(1)
+    end
+  end
+
+  describe '#decrement_comments_counter' do
+    it 'decrements the comments_counter of the post' do
+      comment = Comment.create(text: 'Content', post:, user:)
+      comment.destroy
+      expect(post.comments_counter).to eq(0)
+    end
+  end
 end
