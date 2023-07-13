f90e1010b5f5e7e3f6ccc805b19bb572c5e7fab1
lucascomamala <lucascomamala@gmail.com>
2023-07-12 23:43:13 -0600

    Counters method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,21 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :increment_posts_counter
+  after_destroy :decrement_posts_counter
+
   # Returns the 5 most recent comments for the post
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
