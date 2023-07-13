0cc471afe83e58f1d2978699e4cc024cbbc888bf
lucascomamala <lucascomamala@gmail.com>
2023-07-13 12:24:21 -0600

    Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes
 
+  validates_presence_of :name
+  validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal_to: 0
+
   # Returns the 3 most recent posts for the user
   def recent_posts
     posts.order(updated_at: :desc).limit(3)
