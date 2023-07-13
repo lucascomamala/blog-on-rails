39abd15927ac6637c05b5d3e3a34079212a81852
lucascomamala <lucascomamala@gmail.com>
2023-07-12 23:40:22 -0600

    Recent methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes
+
+  # Returns the 3 most recent posts for the user
+  def recent_posts
+    posts.order(updated_at: :desc).limit(3)
+  end
 end
