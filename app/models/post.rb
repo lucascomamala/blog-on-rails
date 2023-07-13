082bac47f8109156a59e3e025de1f2534c657575
lucascomamala <lucascomamala@gmail.com>
2023-07-13 12:47:21 -0600

    Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ApplicationRecord
   after_save :increment_posts_counter
   after_destroy :decrement_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates_presence_of :content
+  validates_numericality_of :comments_counter, :likes_counter, only_integer: true, greater_than_or_equal_to: 0
+
   # Returns the 5 most recent comments for the post
   def recent_comments
     comments.order(created_at: :desc).limit(5)
