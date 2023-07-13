1ec7699ca045e3ace46bf9c1d5265d5701aa395d
lucascomamala <lucascomamala@gmail.com>
2023-07-13 12:43:32 -0600

    Recent methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  # Returns the 5 most recent comments for the post
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
