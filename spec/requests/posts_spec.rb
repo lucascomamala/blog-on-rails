50167498978e46ce13a57017857ff9d99d34b1ef
lucascomamala <lucascomamala@gmail.com>
2023-07-20 00:35:57 -0600

    Posts request spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /users/:user_id/posts' do
+    before :each do
+      get '//users/:user_id/posts'
+    end
+
+    it 'should have a http status of 200(correct status)' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render post/index view' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include the placeholder text' do
+      expect(response.body).to include('Posts#index')
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    before :each do
+      get '/users/:user_id/posts/:id'
+    end
+
+    it 'should have a http status of 200(correct status)' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render post/show view' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should include the placeholder text' do
+      expect(response.body).to include('Posts#show')
+    end
   end
 end
