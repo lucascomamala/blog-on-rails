829e24fc468937ecb3687cf9a15d5fb77b595a43
lucascomamala <lucascomamala@gmail.com>
2023-07-20 11:58:34 -0600

    Add header

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'users#index'
+  root :to => redirect('users')
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
