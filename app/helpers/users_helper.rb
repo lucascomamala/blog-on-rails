421e82cb5d26b18aa1478032a9c591e9d3e11896
lucascomamala <lucascomamala@gmail.com>
2023-07-20 15:05:54 -0600

    User refactor

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def render_type(user)
+    if current_page?(users_path)
+      link_to(image_tag(user.photo, class: "user-img"), user)
+    else
+      image_tag(user.photo, class: "user-img")
+    end
+  end
 end
