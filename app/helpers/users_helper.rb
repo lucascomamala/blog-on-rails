38c392a3c5cb7cf151c9528e531a0f68650b5132
lucascomamala <lucascomamala@gmail.com>
2023-07-20 15:19:01 -0600

    Add default user img

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def render_type(user)
     if current_page?(users_path)
-      link_to(image_tag(user.photo, class: "user-img"), user)
+      link_to(image_tag(user.photo || "default-user.png", class: "user-img"), user)
     else
-      image_tag(user.photo, class: "user-img")
+      image_tag(user.photo || "default-user.png", class: "user-img")
     end
   end
 end
