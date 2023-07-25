552688db8bd8a21d34f84ea983a066dd31b4200c
lucascomamala <lucascomamala@gmail.com>
2023-07-25 00:23:36 -0600

    Posts spec for new & create

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /users/:user_id/posts' do
+  describe 'GET index' do
     let(:user) { FactoryBot.create(:user) }
     
     before :each do
@@ -17,7 +17,7 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-  describe 'GET /users/:user_id/posts/:post_id' do
+  describe 'GET show' do
     let(:post) { FactoryBot.create(:post) }
     let(:user) { post.author }
 
@@ -32,6 +32,34 @@ RSpec.describe 'Posts', type: :request do
     it 'should render posts/show view' do
       user_post_path(1, 1)
       expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET new' do
+    let(:post) { FactoryBot.create(:post) }
+    let(:user) { post.author }
+
+    before :each do
+      get new_user_post_path(user.id)
+    end
+
+    it 'should have a http status of 200(correct status)' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render posts/new view' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST create' do
+    let(:posty) { FactoryBot.create(:post) }
+    let(:user) { posty.author }
+
+    it 'creates a new Post' do
+      expect do
+        posty.save
+      end.to change(Post, :count).by(1)
     end
   end
 end
