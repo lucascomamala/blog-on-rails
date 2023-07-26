3730313adb19e9b99348860e474a09117574346a
lucascomamala <lucascomamala@gmail.com>
2023-07-26 14:41:45 -0600

    Fix mistakes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -28,11 +28,11 @@ module PostsHelper
 
     if current_page?(user_post_path(post.author, post))
       post.comments.each do |comment|
-        built_comments += "<strong>#{User.find(comment.user_id).name}:</strong> #{comment.content}<br>"
+        built_comments += "<strong>#{User.find(comment.author_id).name}:</strong> #{comment.text}<br>"
       end
     else
       post.recent_comments.each do |comment|
-        built_comments += "<strong>#{User.find(comment.user_id).name}:</strong> #{comment.content}<br>"
+        built_comments += "<strong>#{User.find(comment.author_id).name}:</strong> #{comment.text}<br>"
       end
     end
 
