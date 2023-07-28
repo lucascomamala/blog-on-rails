a31d6f3939c1fd4fdf12dc8a0a6a4ad1fdf89cea
lucascomamala <lucascomamala@gmail.com>
2023-07-28 14:07:44 -0600

    Create comment unfinished

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::CommentsController < Api::V1::ApplicationController
     @comments = Post.find(params[:post_id]).comments
     render json: @comments, status: 200
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params, user_id:, post_id:)
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: { errors: @comment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
